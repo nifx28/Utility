617a56ae06c2c5a88d092c18bf1cb31ce066a00a
nifx28 <nifx28@gmail.com>
2015-12-07 21:55:41 +0800

    Add Unity3D practice.

---
+++ Unity3D/ButtonBehaviourScript.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class NewBehaviourScript1 : MonoBehaviour {
+	private readonly float amount = 20;
+	private static GameObject cube;
+	private static bool isStart = false;
+
+	// Use this for initialization
+	void Start () {
+		if (cube == null) {
+			MeshFilter[] meshes = FindObjectsOfType<MeshFilter> ();
+
+			foreach (MeshFilter mesh in meshes) {
+				if (mesh.sharedMesh.name.Equals ("Cube") && mesh.gameObject.name.Equals ("Water Cube")) {
+					cube = mesh.gameObject;
+				}
+			}
+		}
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (isStart) {
+			cube.transform.Rotate(Vector3.up * amount * Time.deltaTime);
+		}
+	}
+
+	void OnMouseDown() {
+		if (name.Equals ("Start")) {
+			isStart = true;
+		} else if (name.Equals ("Stop")) {
+			isStart = false;
+		} else if (name.Equals ("Reset")) {
+			cube.transform.rotation = new Quaternion();
+		}
+	}
+}
---
+++ Unity3D/CameraBehaviourScript.cs
@@ -0,0 +1,58 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class NewBehaviourScript : MonoBehaviour {
+	private readonly float amount = 20;
+	private Text coord;
+
+	// Use this for initialization
+	void Start () {
+		if (coord == null) {
+			Text[] objs = GameObject.FindObjectsOfType<Text> ();
+
+			foreach (Text obj in objs) {
+				if (obj.name.Equals("Value")) {
+					coord = obj;
+				}
+			}
+		}
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		Vector3 trans = new Vector3 ();
+
+		if (Input.GetKey (KeyCode.W)) {
+			trans = Vector3.forward * amount * Time.deltaTime;
+		}
+
+		if (Input.GetKey (KeyCode.S)) {
+			trans = Vector3.back * amount * Time.deltaTime;
+		}
+
+		if (Input.GetKey (KeyCode.A)) {
+			trans = Vector3.left * amount * Time.deltaTime;
+		}
+
+		if (Input.GetKey(KeyCode.D)) {
+			trans = Vector3.right * amount * Time.deltaTime;
+		}
+
+		if (Input.GetKey(KeyCode.C)) {
+			trans = Vector3.down * amount * Time.deltaTime;
+		}
+
+		if (Input.GetKey(KeyCode.Space)) {
+			trans = Vector3.up * amount * Time.deltaTime;
+		}
+
+		Vector3 pos = transform.position;
+		transform.Translate(trans);
+
+		if (transform.position.y > 10 || transform.position.y < 0.1) {
+			transform.position = pos;
+		}
+
+		coord.text = transform.position.x + "\n" + transform.position.y + "\n" + transform.position.z;
+	}
+}
